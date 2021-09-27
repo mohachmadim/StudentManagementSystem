79451bbe5e68e7b8f3f4072fae81e51d36fe5f5b
mohachmadim <72310049+mohachmadim@users.noreply.github.com>
2021-09-27 11:06:10 +0100

    initial commit

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -1,1 +1,51 @@
+let numberOfStudents = 0;
+
+function addStudent(name){
+    students.push(name);
+    numberOfStudents = students.length;
+}
+
+let students = [];
+
+function clearStudents(name){
+    students = []
+    numberOfStudents = 0;
+}
+
+function createFullName(firstName, lastName){
+    return firstName + ' ' + lastName;
+}
+
+// function getStudentByInitials(name, initial){
+//     return (name[0]==initial);
+// }
+
+function isFullName(name){
+    return typeof(name.split(' ')[1])=='string'
+}
+
+isFullName('John Doe')
+
+function getStudentsByInitials(firstInitial,
+secondInitial)  {
+    let index = 0;
+    let output = [];
+    while(index<students.length-1){
+        if(isFullName(students[index]) &&
+        students[index].split(' ')[0][0] == firstInitial &&
+        students[index].split(' ')[1][0] == secondInitial){
+            output.push(students[index]);
+        }
+        index++;
+    }
+    return output;
+}
+
+addStudent('John Doe');
+addStudent('Josh');
+
+addStudent('Bill Whethers');
+console.log(students);
+console.log(getStudentsByInitials('J','D'));
+
 
